21787aa099692c7f49f8fe5b676aa802af8ccaf4
hanifadzkiya <hanifadzkiya@gmail.com>
2020-03-14 20:36:49 +0700

    ad controls

---
+++ controls.hpp
@@ -0,0 +1,11 @@
+#ifndef CONTROLS_HPP
+#define CONTROLS_HPP
+
+void computeMatricesFromInputs();
+glm::mat4 getViewMatrix();
+glm::mat4 getProjectionMatrix();
+
+glm::vec3 *getPosition();
+void setPosition(glm::vec3 var);
+
+#endif
